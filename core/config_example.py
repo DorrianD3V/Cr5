fe2cf55e92ae4f1b813d2055d5f75a3cf037627a
DorrianD3V <artem129871@gmail.com>
2021-01-06 22:08:10 +0200

    Добавлена возможность изменять ENV через конфиг.

--- core/config_example.py
+++ core/config_example.py
@@ -1,16 +1,25 @@
 
 class Config:
     token = 'Токен бота'
+
     extensions = [
         'jishaku',
         'cogs.*',
         'cogs.internal.*'
     ]
+
     channels = {
         'bugs': 0, # Канал, куда будут отправлятся баги
         'backups': 0, # Канал, куда будут отправлятся дампы PostgreSQL
         'exceptions': 0 # Канал, куда будут отправляться ошибки бота
     }
+
     database = {
         'url': 'postgresql://username:password@127.0.0.1:5432/cr5'
+    }
+
+    environ = {
+        'JISHAKU_NO_UNDERSCORE': '1',
+        'JISHAKU_NO_DM_TRACEBACK': '1',
+        'JISHAKU_HIDE': '1'
     }
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
+import os
+import yaml
+
 sys.dont_write_bytecode = True
 
-
 import discord
-import yaml
 from discord.ext import commands
 
 from core import Bot, Config
@@ -11,6 +12,10 @@ from core import Bot, Config
 
 if __name__ == "__main__":
     config = Config()
+
+    for k, v in config.environ.items():
+        os.environ[k] = v
+
     bot = Bot(config,
               command_prefix='c.',
               enabled_intents=discord.Intents.all(),
