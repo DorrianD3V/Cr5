62a89410edf1e2d179658d00192fe017365c20f8
DorrianD3V <artem129871@gmail.com>
2021-01-07 21:00:19 +0200

    Изменены аргументы в Context.send

--- core/context.py
+++ core/context.py
@@ -4,4 +4,4 @@ from discord.ext import commands
 
 class Context(commands.Context):
     async def send(self, *args, **kwargs):
-        return await self.reply(*args, **kwargs)
+        return await self.reply(*args, **kwargs, mention_author=True)
