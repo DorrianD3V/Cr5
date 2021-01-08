7541f042b47a8e2986c86e674d858f09df0867ee
DorrianD3V <artem129871@gmail.com>
2021-01-08 14:45:42 +0200

    Добавлен метод Context.react

--- core/context.py
+++ core/context.py
@@ -5,3 +5,9 @@ from discord.ext import commands
 class Context(commands.Context):
     async def send(self, *args, **kwargs):
         return await self.reply(*args, **kwargs, mention_author=True)
+
+    async def react(self, *emojis):
+        async def _reaction_add_task():
+            for emoji in emojis:
+                await self.message.add_reaction(emoji)
+        self.bot.loop.create_task(_reaction_add_task())
